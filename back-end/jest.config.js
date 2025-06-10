aa1a0dd47f4a5df0edfdf68ec9e2b2a3b7e91630
marcusvxf <marcusviniciusleite1@hotmail.com>
2025-06-10 20:12:57 -0300

    feat: add base to backend

---
+++ back-end/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.test.ts'],
+  clearMocks: true,
+};
