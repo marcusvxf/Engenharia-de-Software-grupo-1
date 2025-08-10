b6fbca9093462e93a2b9d76af034c1259565bf60
marcusvxf <marcusviniciusleite1@hotmail.com>
2025-08-10 07:48:53 -0300

    feat: add autenticação de usuario

--- back-end/src/controllers/user.controller.ts
+++ back-end/src/controllers/user.controller.ts
@@ -5,12 +5,18 @@ import { UserService } from '../services/user.service';
 const userService = new UserService();
 
 export const getAllUsers = async (req: Request, res: Response) => {
-    const users = await userService.getAll();
-    res.json(users);
+  const users = await userService.getAll();
+  res.json(users);
 };
 
 export const createUser = async (req: Request, res: Response) => {
-    const { name, email } = req.body;
-    const user = await userService.create(name, email);
-    res.status(201).json(user);
+  const { name, email, password } = req.body;
+  const user = await userService.create(name, email, password);
+  res.status(201).json(user);
+};
+
+export const login = async (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  const token = await userService.login(email, password);
+  res.json({ token });
 };
